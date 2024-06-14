1f9a37143e9d591bcb61a72a8c67f0d13862d984
Proyecto-Final-Equipo-4 <proyecto.final.equipo.04@gmail.com>
2024-06-14 18:31:49 -0300

    Update UsuarioServicio.java
    Corrección de sintaxis

--- servicios/src/main/java/com/app/servicios/servicios/UsuarioServicio.java
+++ servicios/src/main/java/com/app/servicios/servicios/UsuarioServicio.java
@@ -30,7 +30,7 @@ public class UsuarioServicio implements UserDetailsService {
 
     @Transactional
     public void registrarCliente(String nombre, String apellido, long id, String direccion, String localidad, String barrio, Integer telefono, String email, String password, String password2) throws MiExcepcion {
-        validar(nombre,apellido,direccion,localidad,barrio,telefono,id, email, password, password2);
+        validarAtributosCliente(nombre,apellido,direccion,localidad,barrio,telefono,id, email, password, password2);
 
         Usuario usuario = new Usuario();
 
@@ -47,60 +47,32 @@ public class UsuarioServicio implements UserDetailsService {
         usuario.setRol(Rol.USUARIO);
 
         usuarioRepositorio.save(usuario);
+        usuarioRepositorio.validarCliente(usuario.getEmail(),password,password2);
     }
 
     private void validarAtributosCliente(String nombre, String apellido, long id, String direccion, String localidad, String barrio, Integer telefono, String email, String password, String password2) throws MiExcepcion {
-        if (nombre.isEmpty() || nombre == null) {
-            throw new MiExcepcion("El nombre no puede ser nulo o estar vacío");
-
-        }
-        if (apellido.isEmpty() || apellido == null) {
-            throw new MiExcepcion("El apellido no puede ser nulo o estar vacío");
-
-        }
-        if (telefono == null) {
-            throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");
-
-        }
-        if (telefono <= 0 ) {
-            throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");
-
-        }
+        if (nombre.isEmpty() || nombre == null) {throw new MiExcepcion("El nombre no puede ser nulo o estar vacío");}
+        if (apellido.isEmpty() || apellido == null) {throw new MiExcepcion("El apellido no puede ser nulo o estar vacío");}
+        if (telefono == null) {throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");}
+        if (telefono <= 0 ) {throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");}
+        
         // Validación que el telefono cumpla con la longitud:
         String telefonoStr = String.valueOf(telefono);
         if (telefonoStr.length()<7 || telefonoStr.length()>10 ) { //El telefono fijo debe tener 7 digitos, y un número de celular contiene 10
-            throw new MiExcepcion("El teléfono debe contener entre 7 y 10 dígitos");
-        }
-        if (email.isEmpty() || email == null) {
-            throw new MiExcepcion("El email no puede ser nulo o estar vacío");
-
-        }
-        if (direccion.isEmpty() || direccion == null) {
-            throw new MiExcepcion("La dirección no puede ser nula o estar vacía");
-
-        }
-        if (localidad.isEmpty() || localidad == null) {
-            throw new MiExcepcion("La localidad no puede ser nula o estar vacía");
-
-        }
-        if (barrio.isEmpty() || barrio == null) {
-            throw new MiExcepcion("El barrio no puede ser nulo o estar vacío");
-
-        }
-        if (password.isEmpty() || password == null || password.length() <= 5) {
-            throw new MiExcepcion("La contraseña no puede estar vacía, y debe tener más de 5 digitos");
-
-        }
-        if (!password.equals(password2)) {
-            throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");
-
-        }
+        throw new MiExcepcion("El teléfono debe contener entre 7 y 10 dígitos");}
+        
+        if (direccion.isEmpty() || direccion == null) {throw new MiExcepcion("La dirección no puede ser nula o estar vacía");}
+        if (localidad.isEmpty() || localidad == null) {throw new MiExcepcion("La localidad no puede ser nula o estar vacía");}
+        if (barrio.isEmpty() || barrio == null) {throw new MiExcepcion("El barrio no puede ser nulo o estar vacío");}
+        if (email.isEmpty() || email == null) {throw new MiExcepcion("El email no puede ser nulo o estar vacío");}
+        if (password.isEmpty() || password == null || password.length() <= 5) {throw new MiExcepcion("La contraseña no puede estar vacía, y debe tener más de 5 digitos");}
+        if (!password.equals(password2)) {throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");}
 
     }
 
     @Transactional
     public void registrarProveedor(String nombre, String apellido, long id, String direccion, String localidad, Set<Servicio> servicios, String barrio, Integer telefono, String email, Integer dni, Integer experiencia, String password, String password2, String descripcion) throws MiExcepcion {
-        validar(nombre,apellido,direccion,localidad,barrio,telefono,id, email, password, password2);
+        validarAtributosProveedor(nombre,apellido,direccion,localidad,barrio,telefono,id, email, password, password2);
 
         Usuario usuario = new Usuario();
 
@@ -117,88 +89,45 @@ public class UsuarioServicio implements UserDetailsService {
         usuario.setDescripcion(descripcion);
         usuario.setServicios(servicios);
 
-        // Se le da el rol de user de manera predeterminada
+        // Se le da el rol de proveedor de manera predeterminada
         usuario.setRol(Rol.PROVEEDOR);
 
         usuarioRepositorio.save(usuario);
+        usuarioRepositorio.validarProveedor(usuario.getEmail(),password, password2);
     }
 
     private void validarAtributosProveedor(String nombre, String apellido, long id, String direccion, String localidad, Set<Servicio> servicios, String barrio, Integer telefono, String email, Integer dni, Integer experiencia, String password, String password2, String descripcion) throws MiExcepcion {
-        if (nombre.isEmpty() || nombre == null) {
-            throw new MiExcepcion("El nombre no puede ser nulo o estar vacío");
-
-        }
-        if (apellido.isEmpty() || apellido == null) {
-            throw new MiExcepcion("El apellido no puede ser nulo o estar vacío");
-
-        }
-        if (telefono == null) {
-            throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");
-
-        }
-        if (telefono <= 0 ) {
-            throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");
-
-        }
+        if (nombre.isEmpty() || nombre == null) { throw new MiExcepcion("El nombre no puede ser nulo o estar vacío");}
+        if (apellido.isEmpty() || apellido == null) {throw new MiExcepcion("El apellido no puede ser nulo o estar vacío");}
+        if (telefono == null) {throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");}
+        if (telefono <= 0 ) {throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");}
+        
         // Validación que el telefono cumpla con la longitud:
         String telefonoStr = String.valueOf(telefono);
         if (telefonoStr.length()<7 || telefonoStr.length()>10 ) { //El telefono fijo debe tener 7 digitos, y un número de celular contiene 10
-            throw new MiExcepcion("El teléfono debe contener entre 7 y 10 dígitos");
-        }
-        if (email.isEmpty() || email == null) {
-            throw new MiExcepcion("El email no puede ser nulo o estar vacío");
+            throw new MiExcepcion("El teléfono debe contener entre 7 y 10 dígitos");}
+        if (email.isEmpty() || email == null) {throw new MiExcepcion("El email no puede ser nulo o estar vacío");}
+        if (direccion.isEmpty() || direccion == null) {throw new MiExcepcion("La dirección no puede ser nula o estar vacía");}
+        if (localidad.isEmpty() || localidad == null) {throw new MiExcepcion("La localidad no puede ser nula o estar vacía");}
+        if (barrio.isEmpty() || barrio == null) {throw new MiExcepcion("El barrio no puede ser nulo o estar vacío");}
+        if (password.isEmpty() || password == null || password.length() <= 5) {throw new MiExcepcion("La contraseña no puede estar vacía, y debe tener más de 5 digitos");}
+        if (!password.equals(password2)) {throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");}
 
-        }
-        if (direccion.isEmpty() || direccion == null) {
-            throw new MiExcepcion("La dirección no puede ser nula o estar vacía");
-
-        }
-        if (localidad.isEmpty() || localidad == null) {
-            throw new MiExcepcion("La localidad no puede ser nula o estar vacía");
-
-        }
-        if (barrio.isEmpty() || barrio == null) {
-            throw new MiExcepcion("El barrio no puede ser nulo o estar vacío");
-
-        }
-        if (password.isEmpty() || password == null || password.length() <= 5) {
-            throw new MiExcepcion("La contraseña no puede estar vacía, y debe tener más de 5 digitos");
-
-        }
-        if (!password.equals(password2)) {
-            throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");
-
-        }
         // Método para validar los atributos específicos según el rol proveedor
         //DNI:
-        if (dni == null) {
-            throw new MiExcepcion("Dato requerido a proveedores");
-
-        }
-        if (dni <= 0 ) {
-            throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");
-
-        }
+        if (dni == null) {throw new MiExcepcion("Dato requerido a proveedores");}
+        if (dni <= 0 ) {throw new MiExcepcion("El teléfono no puede ser nulo o estar vacío");}
         // Validación que el telefono cumpla con la longitud:
         String dniStr = String.valueOf(dni);
         if (dniStr.length()<7 || dniStr.length()>8 ) { 
             throw new MiExcepcion("El dni debe contener entre 7 y 8 dígitos");
         }
-
         //Experiencia:
-
-        if (experiencia == null) {
-            throw new MiExcepcion("Dato requerido a proveedores");
-
-        }
+        if (experiencia == null) {throw new MiExcepcion("Dato requerido a proveedores");}
         if (experiencia <= 0 ) {
-            throw new MiExcepcion("Los Años de experiencia deben ser un número positivo mayor de cero");
-       
-        }
-        if (descripcion.isEmpty() || descripcion == null) {
-            throw new MiExcepcion("Dato requerido a proveedores");
-
-        }
+            throw new MiExcepcion("Los Años de experiencia deben ser un número positivo mayor de cero");}
+        //Descripcion:
+        if (descripcion.isEmpty() || descripcion == null) {throw new MiExcepcion("Dato requerido a proveedores");}
     }
 
     // Nombre de usuario (por mail) para Autenticacion
