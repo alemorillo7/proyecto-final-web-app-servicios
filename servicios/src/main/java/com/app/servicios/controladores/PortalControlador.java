c58de3c10f81ba5081b9c71434ce95ef0a368e4a
Marre Emilio <emilioamarre@gmail.com>
2024-06-15 22:30:48 -0300

    seguridad web, portal controaldor, login y logout, portal admin

--- servicios/src/main/java/com/app/servicios/controladores/PortalControlador.java
+++ servicios/src/main/java/com/app/servicios/controladores/PortalControlador.java
@@ -1,6 +1,7 @@
 package com.app.servicios.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,122 +34,177 @@ public class PortalControlador {
     }
 
     @GetMapping("/registrarProveedor")
-    public String formularioRegistroProveedor(ModelMap model) {
+    public String resgistrarProveedor(ModelMap modelo) {
         List<Servicio> servicios = servicioRepositorio.findAll();
-        model.addAttribute("servicios", servicios);
+        modelo.addAttribute("servicios", servicios);
         return "registroProveedor.html";
     }
 
     @PostMapping("/registroProveedor")
     public String registroProveedor(@RequestParam("email") String email,
-            @RequestParam("nombre") String nombre,
-            @RequestParam("apellido") String apellido,
-            @RequestParam("dni") Integer dni,
-            @RequestParam("direccion") String direccion,
-            @RequestParam("localidad") String localidad,
-            @RequestParam("barrio") String barrio,
-            @RequestParam("servicios") List<String> serviciosIds,
-            @RequestParam("descripcion") String descripcion,
-            @RequestParam("telefono") Integer telefono,
-            @RequestParam("imagen") String imagen,
-            @RequestParam("experiencia") Integer experiencia,
-            @RequestParam("password") String password,
-            @RequestParam("password2") String password2) {
-        if (!password.equals(password2)) {
-            // Manejar el error de contraseñas no coincidentes
-            return "redirect:/registroProveedor";
-        }
-        Usuario proveedor = new Usuario();
-        proveedor.setEmail(email);
-        proveedor.setNombre(nombre);
-        proveedor.setApellido(apellido);
-        proveedor.setDni(dni);
-        proveedor.setDireccion(direccion);
-        proveedor.setLocalidad(localidad);
-        proveedor.setBarrio(barrio);
-        proveedor.setDescripcion(descripcion);
-        proveedor.setTelefono(telefono);
-        proveedor.setImagen(imagen);
-        proveedor.setExperiencia(experiencia);
-        proveedor.setRol(Rol.PROVEEDOR);
-        proveedor.setPassword(password);
-        Set<Servicio> servicios = new HashSet<>();
-        for (String servicioId : serviciosIds) {
-            Servicio servicio = servicioRepositorio.findById(servicioId).orElse(null);
-            if (servicio != null) {
-                servicios.add(servicio);
-            }
-        }
-        proveedor.setServicios(servicios);
+            @RequestParam String nombre,
+            @RequestParam String apellido,
+            @RequestParam Integer dni,
+            @RequestParam String direccion,
+            @RequestParam String localidad,
+            @RequestParam String barrio,
+            @RequestParam List<String> serviciosIds,
+            @RequestParam String descripcion,
+            @RequestParam Integer telefono,
+            @RequestParam String imagen,
+            @RequestParam Integer experiencia,
+            @RequestParam String password,
+            @RequestParam String password2,
+            ModelMap modelo) {
 
+        try {
+            usuarioServicio.registrarProveedor(email, nombre, apellido, dni, direccion, localidad, barrio, serviciosIds,
+                    descripcion, telefono, imagen, experiencia, password, password2);
+            modelo.put("exito", "Se ha registrado como proveedor correctamente");
+            return "index.html:/";
+        } catch (Exception e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("apellido", apellido);
+            modelo.put("dni", dni);
+            modelo.put("direccion", direccion);
+            modelo.put("localidad", localidad);
+            modelo.put("barrio", barrio);
+            modelo.put("serviciosIds", serviciosIds);
+            modelo.put("descripcion", descripcion);
+            modelo.put("telefono", telefono);
+            modelo.put("imagen", imagen);
+            modelo.put("experiencia", experiencia);
+            return "registroProveedor.html";
+        }
+        // Usuario proveedor = new Usuario();
+        // proveedor.setEmail(email);
+        // proveedor.setNombre(nombre);
+        // proveedor.setApellido(apellido);
+        // proveedor.setDni(dni);
+        // proveedor.setDireccion(direccion);
+        // proveedor.setLocalidad(localidad);
+        // proveedor.setBarrio(barrio);
+        // proveedor.setDescripcion(descripcion);
+        // proveedor.setTelefono(telefono);
+        // proveedor.setImagen(imagen);
+        // proveedor.setExperiencia(experiencia);
+        // proveedor.setRol(Rol.PROVEEDOR);
+        // proveedor.setPassword(new BCryptPasswordEncoder().encode(password));
+        // Set<Servicio> servicios = new HashSet<>();
+        // for (String servicioId : serviciosIds) {
+        // Servicio servicio = servicioRepositorio.findById(servicioId).orElse(null);
+        // if (servicio != null) {
+        // servicios.add(servicio);
+        // }
+        // }
+        // proveedor.setServicios(servicios);
+        // usuarioRepositorio.save(proveedor);
+        return "index.html:/";
     }
 
     @GetMapping("/RegistrarCliente")
-    public String formularioRegistroCliente(ModelMap model) {
+    public String registrarCliente() {
         return "registroCliente.html";
 
     }
 
     @PostMapping("/RegistroCliente")
-    public String registroCliente(@RequestParam("email") String email,
-            @RequestParam("nombre") String nombre,
-            @RequestParam("apellido") String apellido,
-            @RequestParam("direccion") String direccion,
-            @RequestParam("localidad") String localidad,
-            @RequestParam("barrio") String barrio,
-            @RequestParam("telefono") Integer telefono,
-            @RequestParam("imagen") String imagen,
-            @RequestParam("password") String password,
-            @RequestParam("password2") String password2) {
+    public String registroCliente(@RequestParam String email,
+            @RequestParam String nombre,
+            @RequestParam String apellido,
+            @RequestParam String direccion,
+            @RequestParam String localidad,
+            @RequestParam String barrio,
+            @RequestParam Integer telefono,
+            @RequestParam String imagen,
+            @RequestParam String password,
+            @RequestParam String password2,
+            ModelMap modelo) {
 
-        if (!password.equals(password2)) {
-            // Manejar el error de contraseñas no coincidentes
-            return "redirect:/registroCliente";
-            Usuario cliente = new Usuario();
-            cliente.setEmail(email);
-            cliente.setNombre(nombre);
-            cliente.setApellido(apellido);
-            cliente.setDireccion(direccion);
-            cliente.setLocalidad(localidad);
-            cliente.setBarrio(barrio);
-            cliente.setTelefono(telefono);
-            cliente.setImagen(imagen);
-            cliente.setRol(Rol.USUARIO);
-            cliente.setPassword(password);
-            usuarioRepositorio.save(cliente);
-            return "redirect:/";
+        try {
+            usuarioServicio.registrarCliente(email, nombre, apellido, direccion, localidad, barrio, telefono, imagen,
+                    password, password2);
+            modelo.put("exito", "Se ha registrado como cliente correctamente");
+            return "index.html:/";
+        } catch (Exception e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("apellido", apellido);
+            modelo.put("direccion", direccion);
+            modelo.put("localidad", localidad);
+            modelo.put("barrio", barrio);
+            modelo.put("telefono", telefono);
+            modelo.put("imagen", imagen);
+            return "registroCliente.html";
         }
+
+        // Usuario cliente = new Usuario();
+        // cliente.setEmail(email);
+        // cliente.setNombre(nombre);
+        // cliente.setApellido(apellido);
+        // cliente.setDireccion(direccion);
+        // cliente.setLocalidad(localidad);
+        // cliente.setBarrio(barrio);
+        // cliente.setTelefono(telefono);
+        // cliente.setImagen(imagen);
+        // cliente.setRol(Rol.USUARIO);
+        // cliente.setPassword(new BCryptPasswordEncoder().encode(password));
+        // usuarioRepositorio.save(cliente);
+
     }
 
     @GetMapping("/RegistrarClienteProveedor")
-    public String formularioRegistroUsuarioProveedor(ModelMap model) {
+    public String registrarClienteProveedor(ModelMap modelo) {
         List<Servicio> servicios = servicioRepositorio.findAll();
-        model.addAttribute("servicios", servicios);
+        modelo.addAttribute("servicios", servicios);
         return "registroClienteProveedor.html";
     }
 
     @PostMapping("/RegistroClienteProveedor")
-    public String registroClienteProveedor(@RequestParam("dni") Integer dni,
-            @RequestParam("servicios") List<String> serviciosIds,
-            @RequestParam("descripcion") String descripcion,
-            @RequestParam("experiencia") Integer experiencia) {
-        Usuario clienteProveedor = new Usuario();
-        clienteProveedor.setDni(dni);
-        clienteProveedor.setDescripcion(descripcion);
-        clienteProveedor.equals(clienteProveedor);
-
-        Set<Servicio> servicios = new HashSet<>();
-        for (String servicioId : serviciosIds) {
-            Servicio servicio = servicioRepositorio.findById(servicioId).orElse(null);
-            if (servicio != null) {
-                servicios.add(servicio);
-            }
+    public String registroClienteProveedor(@RequestParam Integer dni,
+            @RequestParam List<String> serviciosIds,
+            @RequestParam String descripcion,
+            @RequestParam Integer experiencia,
+            ModelMap modelo) {
+        try {
+            usuarioServicio.registrarClienteProveedor(dni, serviciosIds, descripcion, experiencia);
+            modelo.put("exito", "Perfil actualizado a proveedor");
+            return "index.html:/";
+        } catch (Exception e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("dni", dni);
+            modelo.put("serviciosIds", serviciosIds);
+            modelo.put("descripcion", descripcion);
+            modelo.put("experiencia", experiencia);
+            return "registroClienteProveedor.html";
         }
-        clienteProveedor.setServicios(servicios);
+
+        // Usuario clienteProveedor = new Usuario();
+        // clienteProveedor.setDni(dni);
+        // clienteProveedor.setDescripcion(descripcion);
+        // clienteProveedor.equals(clienteProveedor);
+
+        // Set<Servicio> servicios = new HashSet<>();
+        // for (String servicioId : serviciosIds) {
+        // Servicio servicio = servicioRepositorio.findById(servicioId).orElse(null);
+        // if (servicio != null) {
+        // servicios.add(servicio);
+        // }
+        // }
+        // clienteProveedor.setServicios(servicios);
+        // usuarioRepositorio.save(clienteComoProveedor);
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(required = false) String error, ModelMap modelo) {
+        if (error != null) {
+            modelo.put("error", "Usuario o Contraseña inválidos!");
+        }
         return "login.html";
     }
+    @PreAuthorize ("hasAnyRole('ROLE_ADMIN', 'ROLE_PROVEEDOR', 'ROLE_USUARIOPROVEEDOR', 'ROLE_USUARIO')")
+    @GetMapping("/inicio")
+    public String inicio() {
+        return "inicio.html:";
 }
