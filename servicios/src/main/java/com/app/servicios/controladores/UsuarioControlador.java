dce7189fb673ba1f3b4af7974585017f0fd12dcc
Hernan Gonzalez <hernangozalez@gmail.com>
2024-06-17 23:45:11 -0300

    Limpieza

--- servicios/src/main/java/com/app/servicios/controladores/UsuarioControlador.java
+++
@@ -1,5 +0,0 @@
-package com.app.servicios.controladores;
-
-public class UsuarioControlador {
-
-}
--- servicios/src/main/java/com/app/servicios/excepciones/MiExcepcion.java
+++
@@ -1,8 +0,0 @@
-package com.app.servicios.excepciones;
-
-public class MiExcepcion extends Exception{
-    public MiExcepcion(String msg){
-        super(msg);
-    }
-
-}
