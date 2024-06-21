394bca9c81feb9417950f95185f19fda65a8416a
Orihuelaa <francoorihuela2912@gmail.com>
2024-06-20 21:36:23 -0300

    creacion de los controladores de  admin y superadmin y agregacion de algunos metodos

--- servicios/src/main/java/com/app/servicios/servicios/CalificacionServicios.java
+++ servicios/src/main/java/com/app/servicios/servicios/CalificacionServicios.java
@@ -18,8 +18,9 @@ public class CalificacionServicios {
     @Autowired
     private CalificacionRepositorio calificacionRepositorio;
 
-    //Validacion de calificación//
-    public void validarCalificacion(Integer puntaje, String comentario, Usuario cliente, Usuario proveedor) throws MiExcepcion {
+    // Validacion de calificación//
+    public void validarCalificacion(Integer puntaje, String comentario, Usuario cliente, Usuario proveedor)
+            throws MiExcepcion {
         if (puntaje == null || puntaje < 1 || puntaje > 5) {
             throw new MiExcepcion("El puntaje debe ser mayor o igual a 1 y menor o igual a 5");
         }
@@ -33,13 +34,14 @@ public class CalificacionServicios {
             throw new MiExcepcion("El proveedor no puede estar vacío");
         }
     }
-    
-    //Crear una calificación//
+
+    // Crear una calificación//
     @Transactional
-    public void crearCalificacion(Integer puntaje, String comentario, Usuario cliente, Usuario proveedor) throws MiExcepcion{
+    public void crearCalificacion(Integer puntaje, String comentario, Usuario cliente, Usuario proveedor)
+            throws MiExcepcion {
 
         try {
-            validarCalificacion(puntaje,comentario,cliente,proveedor);
+            validarCalificacion(puntaje, comentario, cliente, proveedor);
             Calificacion calificacion = new Calificacion();
             calificacion.setPuntaje(puntaje);
             calificacion.setComentario(comentario);
@@ -47,16 +49,17 @@ public class CalificacionServicios {
             calificacion.setProveedor(proveedor);
             calificacion.setActivo(true);
             calificacionRepositorio.save(calificacion);
-            
+
         } catch (MiExcepcion ex) {
             throw new MiExcepcion(ex.getMessage());
         }
 
     }
 
-    //Modificar una calificación//
+    // Modificar una calificación//
     @Transactional
-    public void modificarCalificacion(String id, Integer puntaje, String comentario, boolean activo) throws MiExcepcion {
+    public void modificarCalificacion(String id, Integer puntaje, String comentario, boolean activo)
+            throws MiExcepcion {
         Calificacion calificacion = new Calificacion();
         calificacion = calificacionRepositorio.findById(id).orElse(null);
         calificacion.setPuntaje(puntaje);
@@ -65,7 +68,7 @@ public class CalificacionServicios {
         calificacionRepositorio.save(calificacion);
     }
 
-    //Desactivar una calificación//
+    // Desactivar una calificación//
     @Transactional
     public void desactivarCalificacion(String id) throws MiExcepcion {
         Calificacion calificacion = new Calificacion();
@@ -74,7 +77,7 @@ public class CalificacionServicios {
         calificacionRepositorio.save(calificacion);
     }
 
-    //Activar una calificación//
+    // Activar una calificación//
     @Transactional
     public void activarCalificacion(String id) throws MiExcepcion {
         Calificacion calificacion = new Calificacion();
@@ -83,41 +86,42 @@ public class CalificacionServicios {
         calificacionRepositorio.save(calificacion);
     }
 
-    //Listado de calificaciones//
-    @Transactional (readOnly = true)
+    // Listado de calificaciones//
+    @Transactional(readOnly = true)
     public List<Calificacion> listarCalificacionesTodos() {
         List<Calificacion> calificacionesTodos = calificacionRepositorio.findAll();
         return calificacionesTodos;
     }
 
-
-    @Transactional (readOnly = true)
+    @Transactional(readOnly = true)
     public List<Calificacion> buscarCalificacionesActivas() {
         List<Calificacion> calificacionesActivas = calificacionRepositorio.buscarCalificacionesActivas();
         return calificacionesActivas;
     }
 
-
-    @Transactional (readOnly = true)
+    @Transactional(readOnly = true)
     public List<Calificacion> buscarCalificacionesInactivas() {
         List<Calificacion> calificacionesInactivas = calificacionRepositorio.buscarCalificacionesInactivas();
         return calificacionesInactivas;
     }
 
-
-    @Transactional (readOnly = true)
+    @Transactional(readOnly = true)
     public List<Calificacion> buscarCalificacionesPorCliente(Usuario cliente) {
         List<Calificacion> calificaciones = new ArrayList<Calificacion>();
         calificaciones = calificacionRepositorio.buscarCalificacionesPorCliente(cliente);
         return calificaciones;
     }
 
-
-    @Transactional (readOnly = true)
+    @Transactional(readOnly = true)
     public List<Calificacion> buscarCalificacionesPorProveedoredor(Usuario proveedor) {
         List<Calificacion> calificaciones = new ArrayList<Calificacion>();
         calificaciones = calificacionRepositorio.buscarCalificacionesPorProveedoredor(proveedor);
         return calificaciones;
+    }
+
+    @Transactional(readOnly = true)
+    public Calificacion buscarCalificacionPorId(String id) {
+        return calificacionRepositorio.findById(id).orElse(null);
     }
 
 }
