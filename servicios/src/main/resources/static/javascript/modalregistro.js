0b3db4fee456a054392064c36b409b5b3ce0cba7
Schreiber <estefania_fefi22@hotmail.com>
2024-06-23 00:05:04 -0300

    estilos y modales

--- servicios/src/main/resources/static/javascript/modalregistro.js
+++ servicios/src/main/resources/static/javascript/modalregistro.js
@@ -3,3 +3,11 @@ $('#exampleModal').on('show.bs.modal', function (e) {
     // Cargar el contenido del formulario desde la otra página
     $modal.find('#modal-body-content').load('formularioCliente.html');
 });
+
+$('#inicioSesion').on('show.bs.modal', function (e) {
+    let $modal = $(this);
+    // Cargar el contenido del formulario desde la otra página
+    $modal.find('#modal-body-content').load('formularioCliente.html');
+});
+
+
