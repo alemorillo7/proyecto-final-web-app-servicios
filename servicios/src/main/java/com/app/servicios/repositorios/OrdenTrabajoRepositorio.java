ec5f6ced460fd88789db089f49d71bf36938262c
Hernan Gonzalez <hernangozalez@gmail.com>
2024-07-01 16:45:02 -0300

    Tablas en bandeja ordenes

--- servicios/src/main/java/com/app/servicios/repositorios/OrdenTrabajoRepositorio.java
+++ servicios/src/main/java/com/app/servicios/repositorios/OrdenTrabajoRepositorio.java
@@ -31,67 +31,67 @@ public interface OrdenTrabajoRepositorio extends JpaRepository<OrdenTrabajo, Str
 
 
     //Query para buscar ordenes ABIERTO PRESUPUESTAR de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_orden = ABIERTO AND ot.estado_trabajo = PRESUPUESTAR")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoOrden = 'ABIERTO' AND ot.estadoTrabajo = 'PRESUPUESTAR'")
     public List<OrdenTrabajo> buscarOrdenesAbiertosPresupuestarCliente(@Param("idCliente") String idCliente);
 
     //Query para buscar ordenes ABIERTO PRESUPUESTAR de proveedores//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_orden = ABIERTO AND ot.estado_trabajo = PRESUPUESTAR")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoOrden = 'ABIERTO' AND ot.estadoTrabajo = 'PRESUPUESTAR'")
     public List<OrdenTrabajo> buscarOrdenesAbiertosPresupuestarProveedor(@Param("idProveedor") String idProveedor);
 
     //Query para buscar ordenes ABIERTO PRESUPUESTADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = PRESUPUESTAR AND ot.estado_orden = ABIERTO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'PRESUPUESTADO' AND ot.estadoOrden = 'ABIERTO'")
     public List<OrdenTrabajo> buscarOrdenesPresupuestadasCliente(@Param("idCliente") String idCliente);
 
     //Query para buscar ordenes ABIERTO PRESUPUESTADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = PRESUPUESTAR AND ot.estado_orden = ABIERTO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'PRESUPUESTADO' AND ot.estadoOrden = 'ABIERTO'")
     public List<OrdenTrabajo> buscarOrdenesPresupuestadasProveedor(@Param("idProveedor") String idProveedor);
 
     //Query para buscar ordenes ABIERTO ACEPTADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = ACEPTADO AND ot.estado_orden = ABIERTO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'ACEPTADO' AND ot.estadoOrden = 'ABIERTO'")
     public List<OrdenTrabajo> buscarOrdenesAceptadasCliente(@Param("idCliente") String idCliente);
 
     //Query para buscar ordenes ABIERTO ACEPTADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = ACEPTADO AND ot.estado_orden = ABIERTO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'ACEPTADO' AND ot.estadoOrden = 'ABIERTO'")
     public List<OrdenTrabajo> buscarOrdenesAceptadasProveedor(@Param("idProveedor") String idProveedor);
 
     //Query para buscar ordenes CERRADO CANCELADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = CANCELADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'CANCELADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesCanceladasCliente(@Param("idCliente") String idCliente);
 
     //Query para buscar ordenes CERRADO CANCELADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = CANCELADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'CANCELADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesCanceladasProveedor(@Param("idProveedor") String idProveedor);
 
     // Query para buscar ordenes CERRADO PRESUPUESTO_RECHAZADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = PRESUPUESTO_RECHAZADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'PRESUPUESTO_RECHAZADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesPresupuestadasRechazadasCliente(@Param("idCliente") String idCliente);
 
     // Query para buscar ordenes CERRADO PRESUPUESTO_RECHAZADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = PRESUPUESTO_RECHAZADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'PRESUPUESTO_RECHAZADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesPresupuestadasRechazadasProveedor(@Param("idProveedor") String idProveedor);
 
     // Query para buscar ordenes CERRADO TRABAJO_RECHAZADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = TRABAJO_RECHAZADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'TRABAJO_RECHAZADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesTrabajoRechazadasCliente(@Param("idCliente") String idCliente);
 
     // Query para buscar ordenes CERRADO TRABAJO_RECHAZADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = TRABAJO_RECHAZADO AND ot.estado_orden = CERRADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'TRABAJO_RECHAZADO' AND ot.estadoOrden = 'CERRADO'")
     public List<OrdenTrabajo> buscarOrdenesTrabajoRechazadasProveedor(@Param("idProveedor") String idProveedor);
 
     // Query para buscar ordenes FINALIZADO CALIFICAR de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = FINALIZADO AND ot.estado_orden = CALIFICAR")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'FINALIZADO' AND ot.estadoOrden = 'CALIFICAR'")
     public List<OrdenTrabajo> buscarOrdenesCalificarCliente(@Param("idCliente") String idCliente);
 
     // Query para buscar ordenes FINALIZADO CALIFICAR de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = FINALIZADO AND ot.estado_orden = CALIFICAR")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'FINALIZADO' AND ot.estadoOrden = 'CALIFICAR'")
     public List<OrdenTrabajo> buscarOrdenesCalificarProveedor(@Param("idProveedor") String idProveedor);
 
     // Query para buscar ordenes CALIFICADO FINALIZADO de clientes//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.cliente = :idCliente AND ot.estado_trabajo = FINALIZADO AND ot.estado_orden = CALIFICADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.cliente = :idCliente AND ot.estadoTrabajo = 'FINALIZADO' AND ot.estadoOrden = 'CALIFICADO'")
     public List<OrdenTrabajo> buscarOrdenesFinalizadasCliente(@Param("idCliente") String idCliente);
 
     // Query para buscar ordenes CALIFICADO FINALIZADO de proveedor//
-    @Query ("SELECT ot FROM orden_trabajo ot WHERE ot.proveedor = :idProveedor AND ot.estado_trabajo = FINALIZADO AND ot.estado_orden = CALIFICADO")
+    @Query ("SELECT ot FROM OrdenTrabajo ot WHERE ot.proveedor = :idProveedor AND ot.estadoTrabajo = 'FINALIZADO' AND ot.estadoOrden = 'CALIFICADO'")
     public List<OrdenTrabajo> buscarOrdenesFinalizadasProveedor(@Param("idProveedor") String idProveedor);
 
 }
